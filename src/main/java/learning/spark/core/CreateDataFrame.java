a9806842b54e3eda04ada965e9d179332d5f3c69
Aadith Ramia <aadith.ramia@gmail.com>
2025-04-16 19:48:28 -0700

    adding CreateDataFrame.java

--- src/main/java/learning/spark/core/CreateDataFrame.java
+++ src/main/java/learning/spark/core/CreateDataFrame.java
@@ -14,12 +14,22 @@ import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
+import org.apache.log4j.Logger;
 
 public class CreateDataFrame {
 
+	private static final Logger logger = Logger.getLogger(CreateDataFrame.class);
+
 	public static void main(String[] args) {
 		
+		
+		String log4jConfigPath = "file:///C:/Users/shravanr/learning/spark/spark_java/log4j.properties";
 
+		
+		System.setProperty("log4j.configuration", log4jConfigPath);
+
+
+		// Create a list of rows
 		  List<Row> rows = Arrays.asList(
                     RowFactory.create(1, "Alice", 12),
                     RowFactory.create(2, "Bob", 23),
@@ -36,9 +46,6 @@ public class CreateDataFrame {
             });
 
         
-			String log4jConfigPath = "file:/C:/Users/shravanr/learning/spark/spark_java/log4j.properties";
-
-			System.setProperty("log4j.configuration", log4jConfigPath);
 
 			SparkConf conf = new SparkConf()
 			.set("spark.driver.extraJavaOptions", "-Dlog4j.configuration=" + log4jConfigPath) //-> this does not work locally. this works 
@@ -49,11 +56,15 @@ public class CreateDataFrame {
 		 try (SparkSession spark = SparkSession.builder()
 		 .appName("CreateDF")
 		 .master("local[*]")
-		 .config(conf)
+		 .config(conf) 
 		 .config("spark.sql.shuffle.partitions", "1")
 		 .getOrCreate()) {
 
-			spark.sparkContext().setLogLevel("ERROR");
+
+			// Log the start of the program
+			logger.warn("created SparkSession");
+
+			//spark.sparkContext().setLogLevel("ERROR");
 			JavaRDD<Row> rdd =	new JavaSparkContext( spark.sparkContext()).parallelize(rows, 2);
 			Dataset<Row> df = spark.createDataFrame(rdd, schema);
 			df.show();
